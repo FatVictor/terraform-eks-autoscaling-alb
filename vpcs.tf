10acfc5fa97b4158a4b6f407e4e0d4d855ac693a
Victor Nguyen <>
2021-12-02 00:35:29 +0700

    # update version

--- vpcs.tf
+++ vpcs.tf
@@ -4,9 +4,9 @@ module "vpc" {
   cidr = "10.0.0.0/16"
   azs = data.aws_availability_zones.available.names
   public_subnets = [
-    "10.0.1.0/24",
-    "10.0.2.0/24",
-    "10.0.3.0/24"]
+    "10.0.64.0/18",
+    "10.0.128.0/18",
+    "10.0.192.0/18"]
   enable_dns_hostnames = true
 
   public_subnet_tags = {
